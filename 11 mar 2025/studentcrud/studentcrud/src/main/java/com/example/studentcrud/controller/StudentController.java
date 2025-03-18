d43badb81f685735e3b565d213de4a4782afb080
Sudeepsnayak <sudeepsnayak100@gmail.com>
2025-03-18 10:21:17 +0530

    files uploaded

--- 11 mar 2025/studentcrud/studentcrud/src/main/java/com/example/studentcrud/controller/StudentController.java
+++ 11 mar 2025/studentcrud/studentcrud/src/main/java/com/example/studentcrud/controller/StudentController.java
@@ -1,4 +1,5 @@
 package com.example.studentcrud.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/users")
 public class StudentController{
+    @Autowired
+    private StudentService studentservice;
 
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
         return "student/index";
     }
 }
